28b2d0658219b52d2d0536b84bd51fa92178206b
Potoofu <abcdhsud@gmail.com>
2021-03-28 20:50:05 +0200

    Add changelog view and load from json model

---
+++ MappingToolsWeb/Classes/Models/ChangelogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MappingToolsWeb.Classes.Models {
+
+    public class ChangelogModel {
+
+        [JsonPropertyName("tag_name")]
+        public string TagName { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("published_at")]
+        public DateTime PublishedAt { get; set; }
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+}
