3432a64bdc072d6a586af2c3c263bd8e18da7d3f
Potoofu <abcdhsud@gmail.com>
2021-04-04 19:57:43 +0200

    Update cache version

--- MappingToolsWeb/wwwroot/service-worker.published.js
+++ MappingToolsWeb/wwwroot/service-worker.published.js
@@ -1,7 +1,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
-const CACHE_VERSION = 1.0;
+const CACHE_VERSION = 1.1;
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
